fa65176a546d88137a6b00bb1cc7d38f0f3ba3c2
RM <rmccauleycode@gmail.com>
2020-01-05 13:28:24 -0600

    version 2

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,49 @@ class Scraper
 
 @@all =[]
  
+def self.scrape_table
+
+ url = 'https://www.thonky.com/pokemon-lets-go/list-of-pokemon-locations'
+ html = open(url)
+ doc = Nokogiri::HTML(html)
+ table = doc.css('table')
+
+  pokemon = []
+    table.search('tr').each do |row|
+     cells = row.search('td')
+
+        pokemon_number = cells[0].to_s#.delete("<td>"),
+        pokemon_name = cells[1].to_s#.downcase.delete("<td>"),
+        pokemon_location = cells[2].to_s#.delete("<td>")   
+        pokemon << {number: pokemon_number, name: pokemon_name, location: pokemon_location }
+      end
+
+    end
+    puts pokemon
+  end
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+=begin
+
+
+
 
 def initialize
   @pokemon = {}
@@ -38,20 +81,29 @@ end
   @@all << self
  end
 
-  table.search('tr').each do |tr|
-   cells = tr.search('td')
-    @pokemon =  {
-      :number => cells[0].to_s.delete("<td>"),
-      :name => cells[1].to_s.downcase.delete("<td>"),
-      :location => cells[2].to_s.delete("<td>") }   
-  
+ 
 
-      #@pokemon.save
-      @@all << @pokemon
+
+
+
+  def sort 
+    @@all.each do |num, nam,
+  
+  
+  def self.locate
+    input = gets.strip.downcase + "/"
+
+    pokemon_values = (@@all.each.map { |info|  [info[:number], info[:name], info[:location] ] } )
+    ans = []
+
+     search = pokemon_values[1].find { |x| x == input }
+    puts pokemon_values
+
+    
+   
   end
-  
       
-  puts @@all  
+  #puts @@all  
+=end
 
-end
 
